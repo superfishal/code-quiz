d90b646c425ba779cd313e559f45cdd2eaa7dee6
superfishal <97133716+superfishal@users.noreply.github.com>
2022-03-07 16:02:39 -0800

    everything but high score submission

--- assets/js/script.js
+++ assets/js/script.js
@@ -4,6 +4,8 @@ var answerEl = document.getElementById("answer");
 var answerSelectEl = document.getElementById("answer-select");
 var startButton = document.getElementById("start");
 var welcomeTitle = document.getElementById("welcome-title");
+var finalScoreEl = document.getElementById("question");
+
 var timerVal;
 var secondsRemaining = 20;
 var currentQuestion = 0;
@@ -20,9 +22,17 @@ var questionArray = [
     choices: ["Swiss", "Jack", "Gambrazola", "Chevre"],
     answer: "Jack",
   },
+  {
+    question: "Is the moon made of cheese?",
+    choices: ["Yes", "No"],
+    answer: "No",
+  },
+  {
+    question: "Which of these is not a cheese?",
+    choices: ["Emmental", "Camembert", "Nooch", "Taleggio"],
+    answer: "Nooch",
+  },
 ];
-
-startButton.addEventListener("click", startQuiz);
 
 function startQuiz() {
   timerEl.innerText = secondsRemaining;
@@ -33,7 +43,7 @@ function startQuiz() {
     } else {
       alert("Time is Up! Post your score!");
       clearInterval(timerVal);
-      // postScore();
+      postScore();
     }
   }, 1000);
   welcomeTitle.classList.add("hide");
@@ -45,34 +55,50 @@ function startQuiz() {
 }
 
 var displayQuestion = function () {
-  // correctAnswer and wrongAnswer function or one with an if else statement...
-  var answerSelect = function () {
-    if (this.value == questionArray[currentQuestion].answer) {
-      console.log(this.value !== questionArray[currentQuestion].answer);
-      secondsRemaining -= 3;
-      timerEl.innerText = secondsRemaining;
-      answerSelectEl.textContent = "Wrong";
-    } else {
-      answerSelectEl.textContent = "Correct";
-    }
-    currentQuestion++;
-    questionEl.textContent = " ";
-    answerEl.textContent = " ";
-    answerSelectEl = " ";
-    displayQuestion();
-  };
+  var questionNumber = questionArray[currentQuestion];
+  console.log(questionNumber.answer);
   // displays first question
-  questionEl.innerText = questionArray[currentQuestion].question;
+  questionEl.innerText = questionNumber.question;
   //loop through the currentQuestion choices and create a button for each one
-  questionArray[currentQuestion].choices.forEach(function (choice) {
+  questionNumber.choices.forEach(function (choices) {
     var choiceButton = document.createElement("button");
     choiceButton.classList.add("my-1", "btn", "btn-primary", "w-50");
-    choiceButton.innerText = choice;
+    choiceButton.innerText = choices;
     choiceButton.onclick = answerSelect;
     answerEl.appendChild(choiceButton);
   });
 };
+// correctAnswer and wrongAnswer function or one with an if else statement...
+var answerSelect = function () {
+  if (this.innerText !== questionArray[currentQuestion].answer) {
+    console.log(questionArray[currentQuestion].answer);
+    secondsRemaining -= 3;
+    timerEl.innerText = secondsRemaining;
+    answerSelectEl.textContent = "Wrong";
+  } else {
+    answerSelectEl.textContent = "Correct";
+  }
+  // flash feedback on page
+  answerSelectEl.setAttribute("class", "feedback");
+  setTimeout(function () {
+    answerSelectEl.setAttribute("class", "feedback hide");
+  }, 1000);
+  console.log(currentQuestion);
+  currentQuestion++;
+
+  questionEl.textContent = " ";
+  answerEl.textContent = " ";
+  if (currentQuestion === questionArray.length) {
+    postScore();
+  } else {
+    displayQuestion();
+  }
+};
+var postScore = function () {
+  clearInterval(timerVal);
+  questionEl.textContent = "Your score is: " + secondsRemaining;
+};
+startButton.addEventListener("click", startQuiz);
 
 // postScore function after Times up Alert
-
 // high scores link either places / appends scores to page or displays an alert box.
